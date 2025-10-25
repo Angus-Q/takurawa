c574f7b99c64b7209aa5cb0410c3983090922f4f
Angus-Q <angus.quigley@icloud.com>
2025-10-25 13:58:19 +1300

    Initial Commit

---
+++ takurawa/takurawaApp.swift
@@ -0,0 +1,17 @@
+//
+//  takurawaApp.swift
+//  takurawa
+//
+//  Created by Angus Quigley on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct takurawaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
